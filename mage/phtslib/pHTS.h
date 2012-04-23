51db786af845258cd27be9cbd1cd5da96a4e1705
Maria-Astrinaki <maria.astrinaki@gmail.com>
2012-04-23 16:18:16 +0200

    Updated license details ~ No source changes

--- mage/phtslib/pHTS.h
+++ mage/phtslib/pHTS.h
@@ -1,39 +1,67 @@
-
-/* ------------------------------------------------------------------------------------------- */
-/*																							   */
-/*  This file is part of MAGE / pHTS (the performative HMM-based speech synthesis system)      */
-/*																							   */
-/*  MAGE / pHTS is free software: you can redistribute it and/or modify it under the terms     */
-/*  of the GNU General Public License as published by the Free Software Foundation, either     */
-/*  version 3 of the license, or any later version.										       */
-/*																							   */
-/*  MAGE / pHTS is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;   */	
-/*  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  */
-/*  See the GNU General Public License for more details.									   */
-/*																							   */	
-/*  You should have received a copy of the GNU General Public License along with MAGE / pHTS.  */ 
-/*  If not, see http://www.gnu.org/licenses/												   */
-/*																							   */
-/*																							   */	
-/*  Copyright 2011 University of Mons :													       */
-/*																							   */	
-/*			Numediart Institute for New Media Art (www.numediart.org)						   */
-/*         Acapela Group (www.acapela-group.com)											   */
-/*																							   */
-/*																							   */
-/*   Developed by :																		       */
-/*																							   */
-/*		Maria Astrinaki, Geoffrey Wilfart, Alexis Moinet, Nicolas d'Alessandro, Thierry Dutoit */
-/*																							   */
-/*																							   */
-/*																							   */
-/*   Copyright 2001-2011 Nagoya Institute of Technology									       */
-/*                    Department of Computer Science										   */
-/*																							   */
-/*   Copyright 2001-2008 Tokyo Institute of Technology										   */
-/*                    Interdisciplinary Graduate School of Science and Engineering			   */
-/*																							   */
-/* ------------------------------------------------------------------------------------------- */
+/* ----------------------------------------------------------------------------------------------- */
+/*                                                                                                 */
+/*  This file is part of MAGE / pHTS (the performative HMM-based speech synthesis system)          */
+/*                                                                                                 */
+/*  MAGE / pHTS is free software: you can redistribute it and/or modify it under the terms         */
+/*  of the GNU General Public License as published by the Free Software Foundation, either         */
+/*  version 3 of the license, or any later version.                                                */
+/*                                                                                                 */
+/*  MAGE / pHTS is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;       */   
+/*  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.      */
+/*  See the GNU General Public License for more details.                                           */
+/*                                                                                                 */   
+/*  You should have received a copy of the GNU General Public License along with MAGE / pHTS.      */
+/*  If not, see http://www.gnu.org/licenses/                                                       */
+/*                                                                                                 */
+/*                                                                                                 */   
+/*  Copyright 2011 University of Mons :                                                            */
+/*                                                                                                 */   
+/*         Numediart Institute for New Media Art Technology (www.numediart.org)	        		   */
+/*         Acapela Group (www.acapela-group.com)                                                   */
+/*                                                                                                 */
+/*                                                                                                 */
+/*   Developed by :                                                                                */
+/*                                                                                                 */
+/*        Maria Astrinaki, Geoffrey Wilfart, Alexis Moinet, Nicolas d'Alessandro, Thierry Dutoit   */
+/*                                                                                                 */
+/*   based on  The HMM-Based Speech Synthesis System (HTS)                                         */
+/*           		  developed by HTS Working Group                                               */
+/*                                                                                                 */
+/*   Copyright 2001-2011 Nagoya Institute of Technology                                            */
+/*                    Department of Computer Science                                               */
+/*                                                                                                 */
+/*   Copyright 2001-2008 Tokyo Institute of Technology                                             */
+/*                    Interdisciplinary Graduate School of Science and Engineering                 */
+/*                                                                                                 */
+/*                                                                                                 */
+/* Redistribution and use in source and binary forms, with or                                      */
+/* without modification, are permitted provided that the following                                 */
+/* conditions are met:                                                                             */
+/*                                                                                                 */
+/* - Redistributions of source code must retain the above copyright                                */
+/*   notice, this list of conditions and the following disclaimer.                                 */
+/* - Redistributions in binary form must reproduce the above                                       */
+/*   copyright notice, this list of conditions and the following                                   */
+/*   disclaimer in the documentation and/or other materials provided                               */
+/*   with the distribution.                                                                        */
+/* - Neither the name of the HTS working group nor the names of its                                */
+/*   contributors may be used to endorse or promote products derived                               */
+/*   from this software without specific prior written permission.                                 */
+/*                                                                                                 */
+/* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND                                          */
+/* CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,                                     */
+/* INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF                                        */
+/* MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE                                        */
+/* DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS                               */
+/* BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,                                        */
+/* EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED                                 */
+/* TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,                                   */
+/* DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON                               */
+/* ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,                                 */
+/* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY                                  */
+/* OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE                                         */
+/* POSSIBILITY OF SUCH DAMAGE.                                                                     */
+/* ----------------------------------------------------------------------------------------------- */
 
 
 #ifndef __pHTS
